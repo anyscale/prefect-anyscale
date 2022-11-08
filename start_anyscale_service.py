34b8856bf14644c9541ccc2aa3ebbda46a2f67ca
Philipp Moritz <pcmoritz@gmail.com>
2022-11-08 12:55:07 -0800

    Put agent file at the right location

--- start_anyscale_service.py
+++ start_anyscale_service.py
@@ -1,6 +1,12 @@
+import os
 import ray
+import shutil
 import subprocess
 
 ray.init()
+
+ANYSCALE_PREFECT_DIR = os.path.dirname(os.path.realpath(__file__))
+
+shutil.copy(os.path.join(ANYSCALE_PREFECT_DIR, "anyscale_prefect_agent.py"), "/home/ray/")
 
 subprocess.check_call(["prefect", "agent", "start", "-q", "test"])
