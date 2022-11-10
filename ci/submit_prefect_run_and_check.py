dc836bd913561266052378dfb0bf860bc9a8a0b9
Philipp Moritz <pcmoritz@gmail.com>
2022-11-09 20:56:26 -0800

    update

--- ci/submit_prefect_run_and_check.py
+++ ci/submit_prefect_run_and_check.py
@@ -1,15 +1,19 @@
+import asyncio
 import uuid
+
 from prefect.deployments import Deployment
 from prefect.client import get_client
 
 run_name = str(uuid.uuid4())
 prefect.deployments.run_deployment("count-to/prefect_test", parameters={"highest_number": 5}, flow_run_name=run_name)
 
-async with get_client() as client:
-    while True:
-        run = await client.read_flow_run(run_name)
-        if run.state.is_completed():
-            break
-        print(run.state)
-        time.sleep(5.0)
+async def wait_for_run_complete(name):
+    async with get_client() as client:
+        while True:
+            run = await client.read_flow_run(name)
+            if run.state.is_completed():
+                break
+            print(run.state)
+            await asyncio.sleep(5.0)
     
+asyncio.run(wait_for_run_complete(run_name))
