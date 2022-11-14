bbbcb14e25b15faadb14f319358a9c2b66557bf7
Philipp Moritz <pcmoritz@gmail.com>
2022-11-14 15:52:09 -0800

    Restart prefect agent if the connection times out due to idleness (#7)

--- start_anyscale_service.py
+++ start_anyscale_service.py
@@ -1,4 +1,5 @@
 import argparse
+import logging
 import os
 import ray
 import shutil
@@ -14,4 +15,11 @@ ANYSCALE_PREFECT_DIR = os.path.dirname(os.path.realpath(__file__))
 
 shutil.copy(os.path.join(ANYSCALE_PREFECT_DIR, "anyscale_prefect_agent.py"), "/home/ray/")
 
-subprocess.check_call(["prefect", "agent", "start", "-q", args.queue])
+# The prefect agent is timing out on the connection to the prefect control
+# plane after about 24h of inactivity. If this happens, restart it.
+while True:
+    logging.info(f"Starting prefect agent for queue {args.queue}")
+    try:
+        subprocess.check_call(["prefect", "agent", "start", "-q", args.queue])
+    except Exception:
+        logging.exception("Restarting prefect agent due to exception")
