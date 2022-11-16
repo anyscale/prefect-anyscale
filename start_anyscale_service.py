751d2d601c682e30fed8b7011a48f071d70e0f5d
Philipp Moritz <pcmoritz@gmail.com>
2022-11-16 13:04:40 -0800

    Use Serve Deployment to run the Agent

--- start_anyscale_service.py
+++ start_anyscale_service.py
@@ -1,9 +1,11 @@
 import argparse
 import logging
 import os
-import ray
 import shutil
 import subprocess
+
+import ray
+from ray import serve
 
 parser = argparse.ArgumentParser()
 parser.add_argument("--queue", type=str)
@@ -11,15 +13,21 @@ args = parser.parse_args()
 
 ray.init()
 
-ANYSCALE_PREFECT_DIR = os.path.dirname(os.path.realpath(__file__))
+serve.start(detached=True)
 
-shutil.copy(os.path.join(ANYSCALE_PREFECT_DIR, "anyscale_prefect_agent.py"), "/home/ray/")
+@serve.deployment
+class PrefectAgentDeployment:
+    def __init__(self):
+        anyscale_prefect_dir = os.path.dirname(os.path.realpath(__file__))
+        shutil.copy(os.path.join(anyscale_prefect_dir, "anyscale_prefect_agent.py"), "/home/ray/")
 
-# The prefect agent is timing out on the connection to the prefect control
-# plane after about 24h of inactivity. If this happens, restart it.
-while True:
-    logging.info(f"Starting prefect agent for queue {args.queue}")
-    try:
-        subprocess.check_call(["prefect", "agent", "start", "-q", args.queue])
-    except Exception:
-        logging.exception("Restarting prefect agent due to exception")
+        # The prefect agent is timing out on the connection to the prefect control
+        # plane after about 24h of inactivity. If this happens, restart it.
+        while True:
+            logging.info(f"Starting prefect agent for queue {args.queue}")
+            try:
+                subprocess.check_call(["prefect", "agent", "start", "-q", args.queue])
+            except Exception:
+                logging.exception("Restarting prefect agent due to exception")
+
+serve.run(PrefectAgentDeployment.bind())
