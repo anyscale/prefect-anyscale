c09e9e0e694d6286544413f9899487bbc706f594
Philipp Moritz <pcmoritz@gmail.com>
2022-11-16 15:50:34 -0800

    Use Serve Deployment to run and health check the Agent (#9)
    This puts the Prefect agent into a Serve Deployment to avoid the application staying in the "Updating" stage while it is running.
    
    It also does proper health checking of the agent and will restart the deployment if the agent fails.

--- start_anyscale_service.py
+++ start_anyscale_service.py
@@ -1,25 +1,36 @@
 import argparse
 import logging
 import os
-import ray
 import shutil
 import subprocess
+
+from fastapi import FastAPI
+import ray
+from ray import serve
 
 parser = argparse.ArgumentParser()
 parser.add_argument("--queue", type=str)
 args = parser.parse_args()
 
-ray.init()
+serve.start(detached=True)
 
-ANYSCALE_PREFECT_DIR = os.path.dirname(os.path.realpath(__file__))
+app = FastAPI()
 
-shutil.copy(os.path.join(ANYSCALE_PREFECT_DIR, "anyscale_prefect_agent.py"), "/home/ray/")
+@serve.deployment(route_prefix="/", num_replicas=1)
+@serve.ingress(app)
+class PrefectAgentDeployment:
+    def __init__(self):
+        anyscale_prefect_dir = os.path.dirname(os.path.realpath(__file__))
+        shutil.copy(os.path.join(anyscale_prefect_dir, "anyscale_prefect_agent.py"), "/home/ray/")
 
-# The prefect agent is timing out on the connection to the prefect control
-# plane after about 24h of inactivity. If this happens, restart it.
-while True:
-    logging.info(f"Starting prefect agent for queue {args.queue}")
-    try:
-        subprocess.check_call(["prefect", "agent", "start", "-q", args.queue])
-    except Exception:
-        logging.exception("Restarting prefect agent due to exception")
+        self.agent = subprocess.Popen(["prefect", "agent", "start", "-q", args.queue])
+
+    @app.get("/healthcheck")
+    def healthcheck(self):
+        poll = self.agent.poll()
+        if poll is None:
+            return
+        else:
+            raise RuntimeError("Prefect agent died")
+
+serve.run(PrefectAgentDeployment.bind())
