ea853d5629ef1f0627b23693c13c78209aae4efd
Philipp Moritz <pcmoritz@gmail.com>
2022-11-16 14:54:40 -0800

    add health check

--- start_anyscale_service.py
+++ start_anyscale_service.py
@@ -4,6 +4,7 @@ import os
 import shutil
 import subprocess
 
+from fastapi import FastAPI
 import ray
 from ray import serve
 
@@ -11,16 +12,25 @@ parser = argparse.ArgumentParser()
 parser.add_argument("--queue", type=str)
 args = parser.parse_args()
 
-ray.init()
-
 serve.start(detached=True)
 
-@serve.deployment
+app = FastAPI()
+
+@serve.deployment(route_prefix="/")
+@serve.ingress(app)
 class PrefectAgentDeployment:
     def __init__(self):
         anyscale_prefect_dir = os.path.dirname(os.path.realpath(__file__))
         shutil.copy(os.path.join(anyscale_prefect_dir, "anyscale_prefect_agent.py"), "/home/ray/")
 
         self.agent = subprocess.Popen(["prefect", "agent", "start", "-q", args.queue])
+
+    @app.get("/healthcheck")
+    def healthcheck(self):
+        poll = self.agent.poll()
+        if poll is None:
+            return
+        else:
+            raise RuntimeError("Prefect agent died")
 
 serve.run(PrefectAgentDeployment.bind())
