36ef2b310c98e0a1110c93da7efec2a13fbb1624
Philipp Moritz <pcmoritz@gmail.com>
2022-11-16 17:32:54 -0800

    Make Prefect Agent log files accessible and pass through the correct environment (#10)

--- start_anyscale_service.py
+++ start_anyscale_service.py
@@ -3,6 +3,7 @@ import logging
 import os
 import shutil
 import subprocess
+import uuid
 
 from fastapi import FastAPI
 import ray
@@ -19,11 +20,19 @@ app = FastAPI()
 @serve.deployment(route_prefix="/", num_replicas=1)
 @serve.ingress(app)
 class PrefectAgentDeployment:
-    def __init__(self):
+    def __init__(self, prefect_env):
         anyscale_prefect_dir = os.path.dirname(os.path.realpath(__file__))
         shutil.copy(os.path.join(anyscale_prefect_dir, "anyscale_prefect_agent.py"), "/home/ray/")
 
-        self.agent = subprocess.Popen(["prefect", "agent", "start", "-q", args.queue])
+        # Logfiles will be closed when the process exits
+        prefect_agent_id = uuid.uuid4()
+        self.logfile_out = open(f"/tmp/prefect-agent-{prefect_agent_id}.out", "w")
+        self.logfile_err = open(f"/tmp/prefect-agent-{prefect_agent_id}.err", "w")
+        self.agent = subprocess.Popen(
+            ["prefect", "agent", "start", "-q", args.queue],
+            env=dict(os.environ, **prefect_env),
+            stdout=self.logfile_out, stderr=self.logfile_err,
+        )
 
     @app.get("/healthcheck")
     def healthcheck(self):
@@ -33,4 +42,7 @@ class PrefectAgentDeployment:
         else:
             raise RuntimeError("Prefect agent died")
 
-serve.run(PrefectAgentDeployment.bind())
+serve.run(PrefectAgentDeployment.bind({
+    "PREFECT_API_URL": os.environ["PREFECT_API_URL"],
+    "PREFECT_API_KEY": os.environ["PREFECT_API_KEY"]
+}))
