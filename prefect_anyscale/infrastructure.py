85e666dc0f0e2ea9b58783c1bf5dd73d90c5817b
Philipp Moritz <pcmoritz@gmail.com>
2022-12-16 17:13:34 -0800

    update

--- prefect_anyscale/infrastructure.py
+++ prefect_anyscale/infrastructure.py
@@ -2,6 +2,7 @@ from typing import Dict, Union
 from typing_extensions import Literal
 
 from prefect.infrastructure.base import Infrastructure
+from prefect.utilities.asyncutils import sync_compatible
 from pydantic import Field
 
 
@@ -11,11 +12,24 @@ class AnyscaleJob(Infrastructure):
         default="anyscale_job", description="The type of infrastructure."
     )
 
-    compute_config: Union[str, Dict[str, str]] = Field(
-        description="Compute config to use for the execution of the job."
+    compute_config: Union[None, str, Dict[str, str]] = Field(
+        description="Compute config to use for the execution of the job.",
+        default=None,
     )
 
-    cluster_env: Union[str, Dict[str, str]] = Field(
+    cluster_env: Union[None, str, Dict[str, str]] = Field(
         description="Cluster environment to use for the execution of the job."
     )
 
+    def preview(self):
+        return " \\\n".join(
+            "compute_config = " + str(self.compute_config),
+            "cluster_env = " + str(self.cluster_env),
+        )
+
+    @sync_compatible
+    async def run(
+        self,
+        task_status = None,
+    ):
+        pass
