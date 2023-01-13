a8f25d9fa96a891ca97ba18bd5dee654ec906ca1
Philipp Moritz <pcmoritz@gmail.com>
2023-01-12 21:37:36 -0800

    Implement Ray Job support

--- prefect_anyscale/infrastructure.py
+++ prefect_anyscale/infrastructure.py
@@ -2,16 +2,28 @@ import logging
 import os
 from typing import Dict, Union
 import subprocess
-import sys
 import tempfile
 
 from prefect.infrastructure.base import Infrastructure, InfrastructureResult
 from prefect.utilities.asyncutils import sync_compatible
 from pydantic import Field
+from ray.job_submission import JobSubmissionClient
 from typing_extensions import Literal
 
 
-class AnyscaleJob(Infrastructure):
+class AnyscaleInfrastructure(Infrastructure):
+
+    def _get_environment_variables(self, include_os_environ: bool = True):
+        os_environ = os.environ if include_os_environ else {}
+        # The base environment must override the current environment or
+        # the Prefect settings context may not be respected
+        env = {**os_environ, **self._base_environment(), **self.env}
+
+        # Drop null values allowing users to "unset" variables
+        return {key: value for key, value in env.items() if value is not None}
+
+
+class AnyscaleJob(AnyscaleInfrastructure):
 
     type: Literal["anyscale-job"] = Field(
         default="anyscale-job", description="The type of infrastructure."
@@ -81,15 +93,52 @@ class AnyscaleJob(Infrastructure):
             status_code=returncode, identifier=""
         )
 
-    def _get_environment_variables(self, include_os_environ: bool = True):
-        os_environ = os.environ if include_os_environ else {}
-        # The base environment must override the current environment or
-        # the Prefect settings context may not be respected
-        env = {**os_environ, **self._base_environment(), **self.env}
-
-        # Drop null values allowing users to "unset" variables
-        return {key: value for key, value in env.items() if value is not None}
 
 class AnyscaleJobResult(InfrastructureResult):
+    """Contains information about the final state of a completed process"""
+    pass
+
+
+class RayJob(AnyscaleInfrastructure):
+
+    type: Literal["ray-job"] = Field(
+        default="ray-job", description="The type of infrastructure."
+    )
+
+    _block_type_name = "Ray Job"
+
+    @sync_compatible
+    async def run(
+        self,
+        task_status = None,
+    ):
+        env = self._get_environment_variables()
+        api_url = env.get("PREFECT_API_URL")
+        api_key = env.get("PREFECT_API_KEY")
+        flow_run_id = env.get("PREFECT__FLOW_RUN_ID")
+
+        cmd = ""
+        if api_url:
+            cmd += "PREFECT_API_URL={}".format(api_url)
+        if api_key:
+            cmd += " PREFECT_API_KEY={}".format(api_key)
+        if flow_run_id:
+            cmd += " PREFECT__FLOW_RUN_ID={}".format(flow_run_id)
+
+        cmd += " /home/ray/anaconda3/bin/python -m prefect.engine"
+
+        # Submit the Job to the local Ray cluster running on the same node:
+        client = JobSubmissionClient("http://127.0.0.1:8265")
+        job_id = client.submit_job(entrypoint=cmd)
+
+        if task_status:
+            task_status.started(job_id)
+
+        return RayJobResult(
+            status_code=0, identifier=job_id
+        )
+
+
+class RayJobResult(InfrastructureResult):
     """Contains information about the final state of a completed process"""
     pass
