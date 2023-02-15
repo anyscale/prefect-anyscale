3afd8c9858066138256eb64ccf72b58fa06b3ad0
Philipp Moritz <pcmoritz@gmail.com>
2023-02-14 19:58:07 -0800

    Update CI to run on new services API (#20)

--- start_anyscale_service.py
+++ start_anyscale_service.py
@@ -5,33 +5,30 @@ import subprocess
 from fastapi import FastAPI
 from ray import serve
 
-parser = argparse.ArgumentParser()
-parser.add_argument("--queue", type=str)
-args = parser.parse_args()
-
 serve.start(detached=True)
 
 app = FastAPI()
 
-@serve.deployment(route_prefix="/", num_replicas=1)
+@serve.deployment(route_prefix="/", num_replicas=1, health_check_period_s=10, health_check_timeout_s=30)
 @serve.ingress(app)
 class PrefectAgentDeployment:
     def __init__(self, prefect_env):
         self.agent = subprocess.Popen(
-            ["prefect", "agent", "start", "-q", args.queue],
+            ["prefect", "agent", "start", "-q", prefect_env["ANYSCALE_PREFECT_QUEUE"]],
             env=dict(os.environ, **prefect_env),
         )
 
-    @app.get("/healthcheck")
-    def healthcheck(self):
+    def check_health(self):
         poll = self.agent.poll()
         if poll is None:
             return
         else:
             raise RuntimeError("Prefect agent died")
 
-serve.run(PrefectAgentDeployment.bind({
+
+entrypoint = PrefectAgentDeployment.bind({
     "PREFECT_API_URL": os.environ["PREFECT_API_URL"],
     "PREFECT_API_KEY": os.environ["PREFECT_API_KEY"],
     "PREFECT_EXTRA_ENTRYPOINTS": "prefect_anyscale",
-}))
+    "ANYSCALE_PREFECT_QUEUE": os.environ["ANYSCALE_PREFECT_QUEUE"],
+})
